1d358df03694104d24d84f1b8f16522cd86b5a25
Yahia Nassab <ynassab@edu.uwaterloo.ca>
2020-10-08 19:15:50 -0400

    Browser UI operates completely as a hand now. Copy to clipboard on-click

--- mtg.js
+++ mtg.js
@@ -21,9 +21,9 @@ async function player_ready() {
 	deck.cards = decklist;
 	deck.card_imgs = []; // store an array of the images associated with each card
 	
-	check = await check_list(); // check the list to make sure there are no errors
+	check = await fetch_list(); // check the list to make sure there are no errors
 	
-	if (check == deck.cards.length) { // called only if check_list checks all cards successfully
+	if (check == deck.cards.length) { // called only if fetch_list checks all cards successfully
 		document.body.innerHTML = '<br> <body> <div id="draw"></div> <br> <div>		<button onclick="reveal(\'draw\')">draw</button> </div> <br> <div id="scry_btn_div"> <button onclick="reveal(\'scry\')">scry</button> </div> <div id = "scry_btn_buffer"> </div> <br> <div id="find_div"> <button onclick="find()">search in deck</button> </div> <br> <div id="add_div"> <button onclick="add()">add to deck</button> </div> <br> <div> <button onclick="shuffle()">shuffle</button> </div> <br> <div id = "load_text"> <br> </div> <br> <div id="scry"></div>'
 		shuffle();
 		document.getElementById('load_text').innerHTML = 'Total cards in deck: ' + deck.cards.length;
@@ -53,26 +53,35 @@ async function player_ready() {
 		}
 	}
 	
-	async function check_list() {
+	async function fetch_list() {
 		var count = 0;
 		try {
 			for (i in deck.cards) {
-				var card = deck.cards[i];
+				let card = deck.cards[i];
 				
 				document.getElementById('load_text').innerHTML = 'Loading ' + card; // show progress
 				
-				let card_url = 'https://api.scryfall.com/cards/named?fuzzy='+card+'&format=image';
-				let response = await fetch(card_url);
-				let blob = await response.blob(); // download as Blob object
+				// check local storage
+				var imgURL = localStorage.getItem(card);
 				
-				if (!response.ok) { // if error encounted in fetch
-					document.getElementById('load_text').innerHTML = 'Unable to identify the card ' + card + '. Please try again.';
-					return null
+				if (imgURL == null) { // fetch image from API
+					// let proxy_url = 'https://cors-anywhere.herokuapp.com/' // rectifies lack of Access-Control-Allow-Origin header in the response
+					let card_url = 'https://api.scryfall.com/cards/named?fuzzy='+card+'&format=image&version=large';
+					let response = await fetch(card_url);
+					var imgURL = await response.url;
+					
+					//save to local storage
+					localStorage.setItem(card, imgURL);
+					
+					if (!response.ok) { // if error encounted in fetch
+						document.getElementById('load_text').innerHTML = 'Unable to identify the card ' + card + '. Please try again.';
+						return null
+					}
 				}
 				
-				deck.card_imgs.push([]) // append an element to the array
-				deck.card_imgs[i][0] = card; //name
-				deck.card_imgs[i][1] = blob; // img
+				deck.card_imgs.push([]); // append an element to the array
+				deck.card_imgs[i][0] = card; // name
+				deck.card_imgs[i][1] = localStorage.getItem(card); // img
 				
 				count += 1;
 			}
@@ -84,6 +93,7 @@ async function player_ready() {
 		}
 	}
 }
+
 
 function URLtext(card_name) {
 	// replace characters for URL
@@ -133,24 +143,51 @@ function display(card, type) {
 	// find the card image
 	for (var i = 0; i < deck.card_imgs.length; i++) {
 		if (deck.card_imgs[i][0] == card) { 
-			var blob = deck.card_imgs[i][1];
-			console.log(blob);
+			var imgURL = deck.card_imgs[i][1];
 		}
 	}
 	
 	card = URLtext(card);
 	
-	var card_size = 240
+	var card_size = 200;
 	var num_cards = document.images.length;
 	var card_buffer = num_cards * card_size
 	
 	let img = document.createElement('img');
 	img.style = "top:150;left:" + card_buffer.toString(10) + ";width:" + card_size.toString(10);
+	img.src = imgURL;
+	img.setAttribute('crossorigin', 'anonymous');
+	if (type == 'draw') {
+		let clock_ = new Date();
+		let img_id = clock_.getTime(); // not fool-proof. Find better way to get ID
+		img.id = img_id;
+		img_id_str = img_id.toString();
+		img.setAttribute("onclick", "pop_img(" + img_id_str + ")");
+	}
 	document.getElementById(type).append(img)
-	
-	img.src = URL.createObjectURL(blob);
-	
 	document.getElementById('load_text').innerHTML = 'Cards remaining: ' + deck.cards.length;
+}
+
+function getImageData(img) {
+	let canvas = document.createElement("canvas");
+	canvas.width = img.width;
+	canvas.height = img.height;
+	let ctx = canvas.getContext("2d");
+	ctx.drawImage(img, 0, 0, img.width, img.height);
+	return canvas
+}
+
+async function pop_img(img_id) {
+	let img = document.getElementById(img_id);
+	let canvas = await getImageData(img); // copy to clipboard
+	async function waitToCopy() {
+		return new Promise((resolve) => {
+			canvas.toBlob(blob => navigator.clipboard.write([new ClipboardItem({'image/png': blob})]));
+			resolve();
+		});
+	}
+	await waitToCopy();
+	img.remove(); // remove img
 }
 
 function place_top() {
@@ -239,12 +276,11 @@ async function add() {
 		var add_card_unformatted = add_input.value;
 		var add_card = URLtext(add_card_unformatted);
 		
-		var blob = await check_in_database();
+		var imgURL = await check_in_database();
 		
-		if (blob != undefined) {
+		if (imgURL != undefined) {
 			deck.cards.unshift(add_card_unformatted); // add to top of decklist
-			deck.card_imgs.push([add_card_unformatted, blob]) // add to image deck
-			console.log(deck.card_imgs);
+			deck.card_imgs.push([add_card_unformatted, imgURL]) // add to image deck
 			document.getElementById('load_text').innerHTML = add_card_unformatted + " successfully placed on top of deck. Shuffle if necessary. Cards remaining: " + deck.cards.length;
 		}
 		else {
@@ -260,13 +296,11 @@ async function add() {
 			//check for card in database
 			let card_url = 'https://api.scryfall.com/cards/named?fuzzy='+add_card+'&format=image';
 			let response = await fetch(card_url);
-			let blob = await response.blob(); // download as Blob object
+			let imgURL = await response.url;
 			
 			document.getElementById('load_text').innerHTML = 'Cards remaining: ' + deck.cards.length;
 			
-			console.log(blob);
-			
-			return blob
+			return imgURL
 		}
 	}
 }
